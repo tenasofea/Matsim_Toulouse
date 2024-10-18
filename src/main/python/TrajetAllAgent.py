c9053d9b57a0fbf28b723cd911dae6fd6a8792ed
tena <154453670+tenasofea@users.noreply.github.com>
2024-10-18 13:50:05 +0200

    Update TrajetAllAgent.py

--- src/main/python/TrajetAllAgent.py
+++ src/main/python/TrajetAllAgent.py
@@ -4,34 +4,32 @@
 import xml.etree.ElementTree as ET
 import gzip
 
-# Fonction pour lire les événements d'un agent spécifique depuis output_events.xml.gz et les écrire dans TrajetAllAgent.xml
+# lire les eventss d'un agent specifique depuis output_events.xml.gz et les ecrire dans TrajetAllAgent.xml
 def ecrire_evenements_agent(fichier_entree, agent_id, fichier_sortie):
     with gzip.open(fichier_entree, 'rt', encoding='utf-8') as f:
-        # Parse le fichier XML d'entrée
+        # parse le fichier XML d'entree
         tree = ET.parse(f)
         root = tree.getroot()
         
-        # Créer la racine pour le nouveau fichier XML
+        # creer la racine pour le nouveau fichier XML
         root_sortie = ET.Element('events')
         
-        # Itérer sur tous les éléments "event"
+        # iterer sur tous les elements "event"
         for event in root.findall('event'):
-            # Vérifier si l'événement correspond à l'agent spécifié
+            # verifier si les events correspond à l'agent specifie
             if (event.get('person') == str(agent_id)) or (event.get('vehicle') == str(agent_id)+':car'):
-                # Ajouter l'événement correspondant à la racine du nouveau fichier XML
+                # Ajouter events correspondant à la racine du nouveau fichier XML
                 root_sortie.append(event)
         
-        # Créer l'arbre avec les événements filtrés
         tree_sortie = ET.ElementTree(root_sortie)
         
-        # Écrire dans le fichier XML de sortie
+        # ecrire dans le fichier XML de sortie
         with open(fichier_sortie, 'wb') as f_out:
             tree_sortie.write(f_out, encoding='utf-8', xml_declaration=True)
 
-# Spécifier le fichier d'entrée, l'ID de l'agent et le fichier de sortie
 fichier_entree = "C:/Users/User/IdeaProjects/matsim-example-project-modified/simulation_output_toulouse/output_events.xml.gz"
-agent_id = "763545"  # Remplacer par l'ID de l'agent souhaité
+agent_id = "763545"  # remplacer par l'ID de l'agent souhaite
 fichier_sortie = "TrajetAllAgent.xml"
 
-# Appeler la fonction pour écrire les événements de l'agent dans le fichier de sortie
+# appeler la fonction pour ecrire les evenements de l'agent dans le fichier de sortie
 ecrire_evenements_agent(fichier_entree, agent_id, fichier_sortie)
